c0fe9e4425470b755e0d5b80ec453691991ffe97
Jun Zhu <junzhu@nxp.com>
2016-03-14 14:57:28 +0800

    nxp-xbmc-setup.sh: Add the Statement of setup
    Signed-off-by: Jun Zhu <junzhu@nxp.com>

--- tools/nxp-xbmc-setup.sh
+++ tools/nxp-xbmc-setup.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # The script will hook the i.MX XBMC layer into the image build
 
+# Statements
+echo -e "\n=============================================================================="
+echo " This is a demostration setup for XBMC on i.MX6 which has a GPU hardware."
+echo " The Demo shows XBMC user interface with Frame Buffer graphical backend."
+echo " As the setup will integrate the COMMERCAIL_WHITE_LIST as ffmpeg/libmad/mpeg2dec,
+echo " the build is limited for evaluation only and has NO Support."
+echo -e "===============================================================================\n "
+
 # Hook the layer into the bblayers.conf
 echo "Start to enable the XBMC layer "
 echo "# For XBMC" >> conf/bblayers.conf
